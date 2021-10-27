4da49eaf84a03516ef5ffc786994db8b1cf49f60
Sudaraka Gamage <ashenchamli220@gmail.com>
2021-10-27 22:52:16 +0530

    Tob Bar Section Final

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,31 +1,42 @@
 import React from 'react'
 import "./topbar.css";
-import { Search, Person } from '@mui/icons-material';
+import { Search, Person, Chat, Notifications } from '@mui/icons-material';
 
 export default function Topbar() {
     return (
         <div className="topbarContainer">
+
             <div className="topbarLeft">
                 <span className="logo">LamaSocial</span>
             </div>
             <div className="topbarCenter">
                 <div className="searchbar">
-                    <Search/>
+                    <Search className="searchIcon"/>
                     <input placeholder="Search for friend, post or video" className="searchInput" />
                 </div>
             </div>
+
             <div className="topbarRight">
                 <div className="topbarLinks">
                     <span className="topbarLink">Homepage</span>
                     <span className="topbarLink">Timeline</span>
                 </div>
-                <div className="topbarIcon">
+                <div className="topbarIcons">
                     <div className="topbarIconItem">
                         <Person/>
                         <span className="topbarIconBadge">1</span>
                     </div>
+                    <div className="topbarIconItem">
+                        <Chat/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Notifications/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
                 </div>
-            </div>
+                <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+            </div>  
             
         </div>
     )
