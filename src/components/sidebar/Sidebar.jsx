77025896070b06d7e983ad656850119777ba0713
Sudaraka Gamage <ashenchamli220@gmail.com>
2021-10-28 23:42:31 +0530

    Feed Section Share Component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -77,6 +77,50 @@ export default function Sidebar() {
                         <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
                         <span className="sidebarFriendName">Jane Doe</span> 
                     </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Jane Doe</span> 
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Jane Doe</span> 
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Jane Doe</span> 
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Jane Doe</span> 
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Jane Doe</span> 
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Jane Doe</span> 
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Jane Doe</span> 
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Jane Doe</span> 
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Jane Doe</span> 
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Jane Doe</span> 
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Jane Doe</span> 
+                    </li>   
                 </ul>
             </div>
         </div>
